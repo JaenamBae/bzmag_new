8d6f10c7ce89368b6670a6641f3b658dbe670352
Jaenam Bae <jaenam@dongyang.ac.kr>
2025-02-01 10:18:23 +0000

    Update TestCaller

--- src/core/testcaller.h
+++ src/core/testcaller.h
@@ -1,10 +1,5 @@
 ﻿#ifndef BZMAG_CORE_UTILITY_UNITTEST_TESTCALLER_H
 #define BZMAG_CORE_UTILITY_UNITTEST_TESTCALLER_H
-/**
-    @ingroup bzmagCoreUnitTest
-    @class bzmag::TestCaller
-    @brief 
-*/
 
 #include "testcase.h"
 #include "testcallerbase.h"
@@ -15,15 +10,15 @@ namespace bzmag
     class TestCaller : public TestCallerBase
     {
     public:
-        typedef typename TestCase<T> MyTestCase;
-        typedef typename MyTestCase::TestMethod MyTestMethod;
+        typedef TestCase<T> MyTestCase;
+        typedef typename MyTestCase::TestMethod MyTestMethod;  // typename 추가
 
     public:
         TestCaller(
             MyTestCase* test_case,
-            MyTestMethod test_method,
+            typename MyTestCase::TestMethod test_method,  // typename 추가
             const char* name);
-        virtual~TestCaller();
+        virtual ~TestCaller();
 
         virtual void call();
         virtual const String& getName() const;
@@ -31,18 +26,18 @@ namespace bzmag
     private:
         String name_;
         MyTestCase* testCase_;
-        MyTestMethod testMethod_;
+        typename MyTestCase::TestMethod testMethod_;  // typename 추가
     };
 
 #include "testcaller.inl"
 
-#define BZMAGUNIT_BEGINE_TESTSUITE(name) typedef name MyTestCase;\
-    typedef bzmag::TestCaller<name> MyTestCaller;\
-    void addTest(MyTestCaller* test) { testCallers_.push_back(test); }\
-    virtual const char* toString() const { return #name; }\
-    name() {
+#define BZMAGUNIT_BEGIN_TESTSUITE(name) typedef name MyTestCase; \
+    typedef bzmag::TestCaller<name> MyTestCaller; \
+    void addTest(MyTestCaller* test) { testCallers_.push_back(test); } \
+    virtual const char* toString() const { return #name; } \
+    public: name() {  // public 추가
 
-#define BZMAGUNIT_TEST(name) addTest(\
+#define BZMAGUNIT_TEST(name) addTest( \
     new MyTestCaller(this, &MyTestCase::name, #name));
 
 #define BZMAGUNIT_END_TESTSUITE() }
